d5795ec5f137f723b547f079ba55803fed47ff7a
rafaelchep <rafaelsvieira777@gmail.com>
2026-04-09 20:38:49 -0300

    cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main(void){
+	//Inicializando Variáveis do Jogador
+	float posX=50, posY=50, spd=4;
+	InitWindow(500, 500, "simple rpg c");
+	SetTargetFPS(60);
+	while(!WindowShouldClose()){
+		ClearBackground(RAYWHITE);
+		BeginDrawing();
+			DrawRectangle(posX, posY, 30, 30, RED);
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
+}
